e41ad8995b8c5aaf527c2e6590fb5ce6374eb92e
5antos <a67984@ualg.pt>
2021-03-22 03:26:04 +0000

    Match input to a list of potential matches

--- didYouMean.js
+++ didYouMean.js
@@ -1,17 +1,18 @@
 /**
  * @author 5antos#4876
  * @param {string} str Input string
- * @param {string[]} array Array of strings corresponding to possible choices
+ * @param {string[]} array Array of strings corresponding to potential matches
  * @param {number} threshold This function will only consider strings with similarity values of <threshold>% or more. Defaults to 60% (recommended)
  * @returns {string|null} String most similar to the input string. Returns null if no string is found according to the threshold value
  */
 
-  function didYouMean(str, array, threshold=60) {
-    return array
-      .map(e => { return {e, v: checkSimilarity(str, e)} }) // checkSimilarity function can be found in this repository
-      .filter(({v}) => v >= threshold/100)
-      .reduce((_, curr, i, arr) => arr[i].v > curr ? arr[i].v : curr.e, null)
-  }
+function didYouMean(str, array, threshold=60) {
+  return array
+    .map(e => { return {e, v: checkSimilarity(str, e)} }) // checkSimilarity function can be found in this repository
+    .filter(({v}) => v >= threshold/100)
+    .reduce((_, curr, i, arr) => arr[i].v > curr ? arr[i].v : curr.e, null)
+}
+
 
 // Example Outputs:
 
