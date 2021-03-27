c832db72cb08b288722f2ca04a5f1640ee5a9b1e
5antos <a67984@ualg.pt>
2021-03-27 17:56:43 +0000

    Convert milliseconds to time units

--- convertMilliseconds.js
+++ convertMilliseconds.js
@@ -10,6 +10,15 @@ function convertMilliseconds(ms) {
   const minutes = ~~(seconds/60)
   const hours = ~~(minutes/60)
   const days = ~~(hours/24)
+
+  return { days, hours: hours%24, minutes: minutes%60, seconds: seconds%60 }
+}
+
+function convertMillisecondsEnhanced(ms) {
+  const seconds = ~~(ms/1000)
+  const minutes = ~~(seconds/60)
+  const hours = ~~(minutes/60)
+  const days = ~~(hours/24)
   const months = ~~(days/30)
   const years = ~~(days/365)
 
@@ -19,7 +28,12 @@ function convertMilliseconds(ms) {
 
 // Example Outputs:
 
-convertMilliseconds(527988000) // { years: 0, months: 0, days: 6, hours: 2, minutes: 39, seconds: 48 }
-convertMilliseconds(1020000) // { years: 0, months: 0, days: 0, hours: 0, minutes: 17, seconds: 0 }
-convertMilliseconds(1.45e10) // { years: 0, months: 5, days: 17, hours: 19, minutes: 46, seconds: 40 }
-convertMilliseconds(1.42006167e12) // { years: 45, months: 7, days: 25, hours: 21, minutes: 34, seconds: 30 }
+convertMilliseconds(527988000) // { days: 6, hours: 2, minutes: 39, seconds: 48 }
+convertMilliseconds(1020000) // { days: 0, hours: 0, minutes: 17, seconds: 0 }
+convertMilliseconds(1.45e10) // { days: 167, hours: 19, minutes: 46, seconds: 40 }
+convertMilliseconds(1.42006167e12) // { days: 16435, hours: 21, minutes: 34, seconds: 30 }
+
+convertMillisecondsEnhanced(527988000) // { years: 0, months: 0, days: 6, hours: 2, minutes: 39, seconds: 48 }
+convertMillisecondsEnhanced(1020000) // { years: 0, months: 0, days: 0, hours: 0, minutes: 17, seconds: 0 }
+convertMillisecondsEnhanced(1.45e10) // { years: 0, months: 5, days: 17, hours: 19, minutes: 46, seconds: 40 }
+convertMillisecondsEnhanced(1.42006167e12) // { years: 45, months: 7, days: 25, hours: 21, minutes: 34, seconds: 30 }
