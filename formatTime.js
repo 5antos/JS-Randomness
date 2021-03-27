b0a0be69a3b045e5a27b58a5ddfcaff538e7f3fc
5antos <a67984@ualg.pt>
2021-03-27 15:41:29 +0000

    Format the time from an object

--- formatTime.js
+++ formatTime.js
@@ -7,14 +7,14 @@
  */
 
 function formatTime(time, format, twoDigits=true) {
-  const formats = { dd:'days', hh:'hours', mm:'minutes', ss:'seconds' }
+  const formats = { YY: 'years', MM: 'months', dd:'days', hh:'hours', mm:'minutes', ss:'seconds' }
 
-  return format.replace(/dd|hh|mm|ss/gi, match => time[formats[match]].toString().padStart(twoDigits ? 2 : 0, '0'))
+  return format.replace(/YY|MM|dd|hh|mm|ss/g, match => time[formats[match]].toString().padStart(twoDigits ? 2 : 0, '0'))
 }
 
 
 // Example Outputs:
 
-formatTime({ days: 6, hours: 2, minutes: 39, seconds: 48 }, 'dd days, hh hours, mm minutes and ss seconds', false) // 6 days, 2 hours, 39 minutes and 48 seconds
+formatTime({ years: 1, months: 3, days: 0, hours: 2, minutes: 39, seconds: 48 }, 'YY year, MM months, dd days, hh hours, mm minutes and ss seconds', false) // 1 year, 3 months, 0 days, 2 hours, 39 minutes and 48 seconds
 formatTime(convertMilliseconds(1020000), 'dd:hh:mm:ss', false) // 0:0:17:0
 formatTime(convertMilliseconds(74000), 'dd:hh:mm:ss') // 00:00:01:14
