d663bec6385f690261ae2274c90e12d240d38235
5antos <a67984@ualg.pt>
2021-03-29 01:22:10 +0100

    Split arrays into chunks

---
+++ chunkArray.js
@@ -0,0 +1,17 @@
+/**
+ * @author 5antos#4876
+ * @param {any[]} array Array to be chunked
+ * @param {number} count Length of each chunk
+ * @returns {any[][]} Chunked array
+ */
+
+ function chunkArray(array, count) {
+  return array.reduce((acc, curr, i, arr) => !(i%count) ? [...acc, arr.slice(i, i+count)] : acc, [])
+}
+
+
+// Example Outputs:
+
+console.log(chunkArray([1,2,3,4,5,6,7,8,9], 3)) // [ [1,2,3], [4,5,6], [7,8,9] ]
+console.log(chunkArray([1,2,3,4,5,6,7,8,9], 4)) // [ [1,2,3,4], [5,6,7,8], [9] ]
+console.log(chunkArray([1,2,3,4,5,6,7,8,9], 5)) // [ [1,2,3,4,5], [6,7,8,9] ]
