f66d694240561e2cee49722e2875659e2c2733f9
5antos <a67984@ualg.pt>
2021-04-14 11:38:23 +0100

    Abbreviate numbers

--- abbreviateNumber.js
+++ abbreviateNumber.js
@@ -1,7 +1,7 @@
 /**
  * @author 5antos#4876
  * @param {number} number Number to abbreviate
- * @param {number} precision Number of decimal digits to be displayed
+ * @param {number} [precision=2] Number of decimal digits to be displayed
  * @returns {string} Abbreviated number
  */
 
