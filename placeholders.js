9c409e107b04f52457b19c9022aa0eb1c2bf2cc8
5antos <a67984@ualg.pt>
2021-04-18 13:42:43 +0100

    Format strings with placeholders

---
+++ placeholders.js
@@ -0,0 +1,17 @@
+/**
+ * @author 5antos#4876
+ * @param {object} placeholders Object containing the strings that should replace each placeholder
+ * @param {string} string String with placeholders
+ * @param {string[]} [delimiters=['{','}']] Array with delimiters for the placeholders
+ * @returns {string} Formatted string
+ */
+
+function applyPlaceholders(placeholders, string, delimiters=['{','}']) {
+  return string.replace(new RegExp(Object.keys(placeholders).map(k => `${delimiters[0]}${k}${delimiters[1]}`).join('|'), 'g'), match => placeholders[match.replace(/\{|\}/g, '')])
+}
+
+
+// Example Outputs:
+
+applyPlaceholders({ name: '5antos', age: 19 }, 'Hello, I\'m {name} and I\'m {age} years old.')    // Hello, I'm 5antos and I'm 19 years old.
+applyPlaceholders({ result: 15 }, '10 + 5 = {{result}}', ['{{', '}}'])                            // 10 + 5 = 15
