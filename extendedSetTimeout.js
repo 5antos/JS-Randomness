1fe81bcd48513b7d7e374485fa658abe3d282699
5antos <a67984@ualg.pt>
2021-07-07 06:22:43 +0100

    Enhancement for the in-built setTimeout() method

--- extendedSetTimeout.js
+++ extendedSetTimeout.js
@@ -1,6 +1,6 @@
 /**
  * This method comes handy when you need to create time outs with a duration bigger than the maximum allowed
- * duration of the NodeJS in-built method setTimeout (2147483647 = (2^31)-1)
+ * duration for the NodeJS in-built method setTimeout (2147483647 = (2^31)-1)
  * 
  * @author 5antos#4876
  * @param {function} callback Function to run when the timer ends
@@ -19,4 +19,4 @@ function extendedSetTimeout(callback, ms) {
 // Example Outputs (extendedSetTimeout VS setTimeout):
 
 extendedSetTimeout(() => console.log('5 years later!'), 1.5778463e11) // [👍] Outputs '5 years later!' after 5 years... if your application remains online the entire time
-setTimeout(() => console.log('5 years later?'),         1.5778463e11) // [👎] Outputs a warning because the provided duration exceeds the maximum allowed duration
+setTimeout(() => console.log('5 years later?'),         1.5778463e11) // [👎] Outputs a warning because the provided duration exceeds the maximum allowed duration for this method
