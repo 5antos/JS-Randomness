3514aa8dcc0201b63e42836e2803fbf7a0e772f7
5antos <a67984@ualg.pt>
2021-07-07 06:00:03 +0100

    Convert milliseconds to time units

--- formatTime.js
+++ formatTime.js
@@ -16,7 +16,7 @@ function autoFormatTime(time) {
   return Object.entries(time)
     .filter(e => e[1])
     .map(e => ([e[0].slice(0, -1).padEnd(e[1] > 1 ? e[0].length : 0, 's'), e[1]]))
-    .map((e,i,arr) => (i === arr.length-1 && arr.length > 1) ? `and ${e[1]} ${e[0]}` : (i === arr.length-2 || arr.length === 1) ? `${e[1]} ${e[0]}` : `${e[1]} ${e[0]},`)
+    .map((e,i,a) => (i === a.length-1 && a.length > 1) ? `and ${e[1]} ${e[0]}` : (i === a.length-2 || a.length === 1) ? `${e[1]} ${e[0]}` : `${e[1]} ${e[0]},`)
     .join(' ')
     || '0 seconds'
 }
