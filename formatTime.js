0b2b5845fb0ba0a0eda8125e462a5292df488d8b
5antos <a67984@ualg.pt>
2021-07-07 17:38:56 +0100

    Convert milliseconds to time units

--- formatTime.js
+++ formatTime.js
@@ -11,7 +11,7 @@ function formatTime(time, format='dd:hh:mm:ss') {
   
   const newFormat = format
     .replace(/dd|hh|mm|ss/g, match => time[formats[match]].toString().padStart(2, '0'))
-    .replace(/^(00?:)+/g, '')
+    .replace(/^(00:)+/g, '')
 
   return newFormat.length > 2 ? newFormat : '00:'+newFormat
 }
