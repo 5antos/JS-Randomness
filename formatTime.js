b6f102ee66882cdfc9f398eabd9d09344cc5757f
5antos <a67984@ualg.pt>
2021-07-07 17:38:05 +0100

    Convert milliseconds to time units

--- formatTime.js
+++ formatTime.js
@@ -1,15 +1,19 @@
 /**
  * @author 5antos#4876
  * @param {object} time Object returned from the convertMilliseconds function (available in this repository) or equal
- * @param {string} format Format to display
+ * @param {string} [format="dd:hh:mm:ss"] Format to display
  * @param {boolean} [twoDigits=true] Whether or not to add zeros on the left of a number if it's lower than 10
  * @returns {string} Formatted string
  */
 
-function formatTime(time, format, twoDigits=true) {
+function formatTime(time, format='dd:hh:mm:ss') {
   const formats = { dd:'days', hh:'hours', mm:'minutes', ss:'seconds' }
+  
+  const newFormat = format
+    .replace(/dd|hh|mm|ss/g, match => time[formats[match]].toString().padStart(2, '0'))
+    .replace(/^(00?:)+/g, '')
 
-  return format.replace(/dd|hh|mm|ss/g, match => time[formats[match]].toString().padStart(twoDigits ? 2 : 0, '0'))
+  return newFormat.length > 2 ? newFormat : '00:'+newFormat
 }
 
 function autoFormatTime(time) {
@@ -24,11 +28,12 @@ function autoFormatTime(time) {
 
 // Example Outputs:
 
-formatTime({ days: 0, hours: 2, minutes: 39, seconds: 48 }, 'dd day(s), hh hour(s), mm minute(s), ss second(s)', false) // 0 day(s), 2 hour(s), 39 minute(s), 48 second(s)
-formatTime(convertMilliseconds(1020000), 'dd:hh:mm:ss', false) // 0:0:17:0
-formatTime(convertMilliseconds(74000), 'hh:mm:ss') // 00:01:14
+formatTime(convertMilliseconds(1020000)) // 17:00
+formatTime(convertMilliseconds(6350676)) // 01:45:50
+formatTime(convertMilliseconds(0)) // 00:00
+formatTime({ days: 0, hours: 8, minutes: 0, seconds: 15 }) // 08:00:15
 
-autoFormatTime({ days: 0, hours: 1, minutes: 30, seconds: 0 }) // 1 hour and 30 minutes
 autoFormatTime(convertMilliseconds(104_490_000)) // 1 day, 5 hours, 1 minute and 30 seconds
-autoFormatTime({ days: 14, hours: 8, minutes: 0, seconds: 1 }) // 14 days, 8 hours and 1 second
 autoFormatTime(convertMilliseconds(0)) // 0 seconds
+autoFormatTime({ days: 0, hours: 1, minutes: 30, seconds: 0 }) // 1 hour and 30 minutes
+autoFormatTime({ days: 14, hours: 8, minutes: 0, seconds: 1 }) // 14 days, 8 hours and 1 second
