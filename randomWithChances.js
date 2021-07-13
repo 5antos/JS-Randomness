738e791a60e180d5997baecfc1b040b4f37c4d4a
5antos <a67984@ualg.pt>
2021-07-13 13:27:11 +0100

    Get a random element from a provided array based on chances

--- randomWithChances.js
+++ randomWithChances.js
@@ -1,13 +1,13 @@
 /**
  * @author 5antos#4876
- * @param {object[]} array Array with objects with a key named 'weight', representing the chance of that element to occur
- * @note An element with a weight value of zero (0) will never occur
+ * @param {object[]} array Array with objects with a key named 'chance', representing the chance of that element to occur
+ * @note An element with a chance value of zero (0) will never occur
  * @returns {any} Random element from the provided array, based on the different elements' chances
  */
 
 function getRandom(array) {
   const auxArray = Array.from(array)
-  auxArray.reduce((acc, curr, i, a) => a[i] = acc + curr.weight, 0)
+  auxArray.reduce((acc, curr, i, a) => a[i] = acc + curr.chance, 0)
 
   return array[auxArray.findIndex(w => w > Math.random()*auxArray[auxArray.length-1])]
 }
@@ -16,13 +16,13 @@ function getRandom(array) {
 // Usage Examples:
 
 getRandom([
-  { rarity: 'Common', weight: 60 },
-  { rarity: 'Uncommon', weight: 25 },
-  { rarity: 'Rare', weight: 10 },
-  { rarity: 'Very rare', weight: 5 }
+  { rarity: 'Common', chance: 60 },
+  { rarity: 'Uncommon', chance: 25 },
+  { rarity: 'Rare', chance: 10 },
+  { rarity: 'Very rare', chance: 5 }
 ])
 
 getRandom([
-  { side: 'Heads', weight: 50 },
-  { side: 'Tails', weight: 50 }
+  { side: 'Heads', chance: 50 },
+  { side: 'Tails', chance: 50 }
 ])
