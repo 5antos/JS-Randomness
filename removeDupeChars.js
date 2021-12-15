9a806348d075d79d6987788f4a679dc66c419953
Giulian Moitinho <66143257+procrastinar@users.noreply.github.com>
2021-12-15 18:38:04 -0300

    update: add 'maxAllowed' option to removeDupeChars

--- removeDupeChars.js
+++ removeDupeChars.js
@@ -1,18 +1,22 @@
 /**
  * @author 5antos#4876
+ * @author Gian
  * @note Uppercase and lowercase letters are considered different characters
  * @param {string} string Input string to remove duplicated characters in a row
- * @param {boolean} [bool=false] Whether or not to allow two letters in a row. Useful for some words, like 'really', 'see', 'good', etc.
+ * @param {number} [maxAllowed = 1] The max duplicated characters allowed in a row
  * @returns {string} String without duplicated characters in a row
  */
 
-function removeDupeChars(string, bool=false) {
-  return string.split('').filter((_, i, a) => a[i] != a[bool ? i+2 : i+1]).join('')
+function removeDupeChars(content, maxAllowed = 1) {
+    return content.split('').filter((_, i, a) => 
+        a[i] != a[i + ((isNaN(maxAllowed) 
+        || maxAllowed > content.length 
+        || maxAllowed < 1) ? 1 : maxAllowed)]).join('')
 }
 
 
 // Example Outputs:
 
 removeDupeChars('Heeeeeyyyy guyssssss!!!!') // Hey guys!
-removeDupeChars('Wow, that code is really short!', true) // Wow, that code is really short!
+removeDupeChars('Wow, that code is really short!', 2) // Wow, that code is really short!
 removeDupeChars('Ssssshhhhh, be quiet please!') // Ssh, be quiet please!
