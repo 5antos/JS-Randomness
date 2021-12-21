503e565e3d8ef35b31c46519c8fbe0235885a398
5antos <a67984@ualg.pt>
2021-12-21 17:33:46 +0000

    Abbreviate numbers

--- abbreviateNumber.js
+++ abbreviateNumber.js
@@ -15,6 +15,6 @@ function abbreviateNumber(number, precision=2) {
 abbreviateNumber(30600000)                  // 30.60M
 abbreviateNumber(500)                       // 500
 abbreviateNumber(175050)                    // 175.05K
-abbreviateNumber(5006500000)              // 5.01B
+abbreviateNumber(5006500000)                // 5.01B
 abbreviateNumber(5006600000, 3)             // 5.007B
 abbreviateNumber(15000)                     // 15k
