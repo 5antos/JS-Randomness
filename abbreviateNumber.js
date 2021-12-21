8947d5bb6fecbe1e1f4b91da7b71bd687ae2eae5
5antos <a67984@ualg.pt>
2021-12-21 17:25:31 +0000

    Abbreviate numbers

--- abbreviateNumber.js
+++ abbreviateNumber.js
@@ -6,21 +6,15 @@
  */
 
 function abbreviateNumber(number, precision=2) {
-  const suffsFromZeros = { 0:'', 3:'k', 6:'M', 9:'G', 12:'T' }
-  const { length } = number.toString()
-  const lengthThird = length%3
-  const divDigits = length-(lengthThird || lengthThird+3)
-  const calc = ''+(number/(10**divDigits)).toFixed(precision)
-
-  return number < 1000 ? ''+number : (calc.indexOf('.') === calc.length-3 ? calc.replace(/\.00/, '') : calc)+suffsFromZeros[divDigits]
+  return number.toLocaleString('en-US', { notation: 'compact', maximumFractionDigits: precision })
 }
 
 
 // Example Outputs:
 
-abbreviateNumber(30600000)       // 30.60M
-abbreviateNumber(500)            // 500
-abbreviateNumber(175050)         // 175.05k
-abbreviateNumber(5006500000)     // 5.01G
-abbreviateNumber(5006600000, 3)  // 5.007G
-abbreviateNumber(15000)          // 15k
+console.log(abbreviateNumber(30600000))                  // 30.6M
+console.log(abbreviateNumber(500))                       // 500
+console.log(abbreviateNumber(175050))                    // 175.05K
+console.log(abbreviateNumber(5006500000))               // 5.01B
+console.log(abbreviateNumber(5006600000, 3))   // 5.007B
+console.log(abbreviateNumber(15000))                     // 15k
