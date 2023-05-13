ae2a2fd54fe3fa3b1bc4afffdf505ad1a20d6950
Pedro Santos <66580910+5antos@users.noreply.github.com>
2023-05-13 05:14:50 +0100

    Run an action a specific amount of times, timed or not

--- repeatAction.js
+++ repeatAction.js
@@ -2,19 +2,25 @@
  * @author 5antos#4876
  * @param {number} amount Amount of times to run the callback
  * @param {function} callback Function to run every loop
+ * @param {function} finishCallback Function to run after running all loops
  * @param {number} time Interval time, in milliseconds (defaults to 0, running every millisecond)
  * @returns {unknown}
  */
 
-function repeatAction(amount, callback, time=0) {
+function repeatAction(amount, callback, finishCallback, time=0) {
   if (amount--) callback()
-  if (amount > 0) return setTimeout(() => repeatAction(amount, callback, time), time)
+  if (amount > 0) return setTimeout(() => repeatAction(amount, callback, finishCallback, time), time)
   
   // Section for code to be executed when the action has repeated the provided amount of times
-  console.log('You found a bone!')
+  finishCallback()
 }
 
 
 // Usage Examples:
 
-repeatAction(5, () => console.log('Digging...'), 3000)
+repeatAction(
+  5,
+  () => console.log('Digging...'),
+  () => console.log('You found a bone!')
+  3000
+)
